43c827d0408054c5e50ea8a137d3dfe48ff7f877
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-02 02:06:44 +0100

    title_added

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: "AppTitle",
+    data: () => ({
+        exMessage: "La mia prima app con Vite!"
+    })
+
+}
+</script>
+
+<template>
+    <h1>{{ exMessage }}</h1>
+</template>
+
+<style>
+h1 {
+    text-align: center;
+}
+</style>
