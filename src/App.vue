13957d974afe1752a61e971cb2ada3b65119b2e4
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-02-02 02:43:44 +0100

    jumbotron_added_bonus

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import AppTitle from "./components/AppTitle.vue"
+import AppHero from "./components/AppHero.vue"
 
 </script>
 
 <template>
   <header>
     <AppTitle />
+    <AppHero />
   </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  margin: 0;
+}
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: "AppHero",
+    data: () => ({
+        image_name: "milano_skyline"
+    }),
+    methods: {
+        createImagePath(image_name) {
+            const url = new URL(`../assets/${image_name}.jpg`, import.meta.url);
+            return url.href
+        }
+    }
+
+}
+</script>
+
+<template>
+    <figure>
+        <img :src="createImagePath(image_name)" :alt="image_name">
+    </figure>
+</template>
+
+<style>
+figure {
+    margin: 0;
+    text-align: center;
+    background-color: blanchedalmond;
+}
+</style>
